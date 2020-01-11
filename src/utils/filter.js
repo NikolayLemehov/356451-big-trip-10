398e97e861275a087013f5c4fb455da801d007cc
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-01-11 12:53:27 +0200

    Учит функции getFutureEvents и getPastEvents фильтровать

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,21 +1,22 @@
 import {FilterType} from "../const";
 
-const getFutureEvents = (events) => {
-  return events;
+const getFutureEvents = (events, date) => {
+  return events.filter((it) => it.date.start > date);
 };
 
-const getPastEvents = (events) => {
-  return events;
+const getPastEvents = (events, date) => {
+  return events.filter((it) => it.date.start <= date);
 };
 
 const getEventsByFilter = (events, filterType) => {
+  const nowDate = new Date();
   switch (filterType) {
     case FilterType.EVERYTHING:
       return events;
     case FilterType.FUTURE:
-      return getFutureEvents(events);
+      return getFutureEvents(events, nowDate);
     case FilterType.PAST:
-      return getPastEvents(events);
+      return getPastEvents(events, nowDate);
   }
   return false;
 };
